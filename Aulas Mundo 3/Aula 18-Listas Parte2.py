fa2f480b00a22b354c5200329ba7bb180fdef610
Fabrício Vale <fabriciovale18@gmail.com>
2021-09-20 21:47:57 -0300

    Aulas e Exercícios

---
+++ Aulas Mundo 3/Aula 18-Listas Parte2.py
@@ -0,0 +1,24 @@
+# galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+# print(galera[2][1])
+#
+# for p in galera:
+#     print(f'{p[0]} tem {p[1]} anos de idade')
+#
+# galera = list()
+# dado = list()
+# for c in range(0, 3):
+#     dado.append(str(input('Nome: ')))
+#     dado.append(int(input('Idade: ')))
+#     galera.append(dado[:])
+#     dado.clear()
+# print(galera)
+#
+# totmai = totmen = 0
+# for p in galera:
+#     if p[1] >= 21:
+#         print(f'{p[0]} é maior de idade.')
+#         totmai += 1
+#     else:
+#         print(f'{p[0]} é menor de idade.')
+#         totmen += 1
+# print(f'Temos {totmai} maiores e {totmen} menores de idade.')
